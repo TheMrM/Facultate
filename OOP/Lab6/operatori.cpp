4c1895f562814811b284e2d5267cc8dc2d4084eb
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-11 04:14:15 +0200

    Update operatori.cpp

--- OOP/Lab6/operatori.cpp
+++ OOP/Lab6/operatori.cpp
@@ -4,72 +4,51 @@ using namespace std;
 
 class Complex {
 private:
-    float Re;
-    float Im;
+    double real;
+    double imag;
 
 public:
-    Complex(float real = 0.0, float imaginary = 0.0) : Re(real), Im(imaginary) {}
+    Complex() : real(0.0), imag(0.0) {}
+    Complex(double r, double i) : real(r), imag(i) {}
 
-    Complex operator + (const Complex& other) {
-        Complex result;
-        result.Re = Re + other.Re;
-        result.Im = Im + other.Im;
-        return result;
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, imag + other.imag);
     }
 
-    Complex operator - (const Complex& other) {
-        Complex result;
-        result.Re = Re - other.Re;
-        result.Im = Im - other.Im;
-        return result;
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, imag - other.imag);
     }
 
-    Complex operator * (const Complex& other) {
-        Complex result;
-        result.Re = Re * other.Re - Im * other.Im;
-        result.Im = Re * other.Im + Im * other.Re;
-        return result;
+    Complex& operator=(const Complex& other) {
+        if (this != &other) {
+            real = other.real;
+            imag = other.imag;
+        }
+        return *this;
     }
 
-    Complex operator / (const Complex& other) {
-        Complex result;
-        float denominator = other.Re * other.Re + other.Im * other.Im;
-        result.Re = (Re * other.Re + Im * other.Im) / denominator;
-        result.Im = (Im * other.Re - Re * other.Im) / denominator;
-        return result;
-    }
-
-    void display() {
-        cout << Re << " + " << Im << "i" << endl;
+    void display() const {
+        cout << "(" << real << " + " << imag << "i)" << endl;
     }
 };
 
 int main() {
-    Complex c1(3, 4);
-    Complex c2(1, 2);
-    Complex c3, c4, c5, c6;
 
-    cout << "c1: ";
-    c1.display();
+    Complex c1(2.0, 3.0);
+    Complex c2(1.0, 4.0);
 
-    cout << "c2: ";
-    c2.display();
+    Complex sum = c1 + c2;
+    cout << "+: ";
+    sum.display();
 
-    c3 = c1 + c2;
-    cout << "Adunare: ";
-    c3.display();
+    Complex difference = c1 - c2;
+    cout << "-: ";
+    difference.display();
 
-    c4 = c1 - c2;
-    cout << "Scadere: ";
-    c4.display();
-
-    c5 = c1 * c2;
-    cout << "Inmultire: ";
-    c5.display();
-
-    c6 = c1 / c2;
-    cout << "Impartire: ";
-    c6.display();
+    Complex assigned;
+    assigned = c1;
+    cout << "=: ";
+    assigned.display();
 
     return 0;
 }
