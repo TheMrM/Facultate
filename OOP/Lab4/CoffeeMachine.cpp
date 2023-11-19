ae209aa7f5d054d391c75c0f142b44c517c1dc50
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-19 13:58:46 +0200

    Update CoffeeMachine + readme.md section

--- OOP/Lab4/CoffeeMachine.cpp
+++ OOP/Lab4/CoffeeMachine.cpp
@@ -1,93 +1,120 @@
 #include <iostream>
+#include <iomanip>
+#include <sstream>
 #include <string>
-
 using namespace std;
 
 class CoffeeItem {
 public:
     CoffeeItem(string name, string size, double price) : name(name), size(size), price(price) {}
 
-    string getName() {
-        return name;
-    }
-
-    double getPrice() {
-        return price;
-    }
-
-    string getSize() {
-        return size;
-    }
+    string getName() const { return name; }
+    double getPrice() const { return price; }
+    string getSize() const { return size; }
+    void setPrice(double newPrice) { price = newPrice; } 
 
 private:
-    string name;
-    string size;
+    string name, size;
     double price;
 };
 
 class Customer {
+public:
+    Customer(string name, int loyaltyPoints) : name(name), loyaltyPoints(loyaltyPoints), totalAmountSpent(0.0), coffeeItem("Unknown", "Unknown", 0.0) {}
+
+    string getName() const { return name; }
+    int getLoyaltyPoints() const { return loyaltyPoints; }
+    double getTotalAmountSpent() const { return totalAmountSpent; }
+    string getCoffeeItemName() const { return coffeeItem.getName(); }
+
+    void addLoyaltyPoints(int points) { loyaltyPoints += points; }
+    void addToTotalAmountSpent(double amount) { totalAmountSpent += amount; }
+    friend void placeOrder(Customer& customer, CoffeeItem& coffee);
+    friend void LoyaltyPointsChecker(Customer& customer, CoffeeItem& coffee); 
 private:
     string name;
     int loyaltyPoints;
     double totalAmountSpent;
     CoffeeItem coffeeItem;
-
-public:
-    Customer(string name, int loyaltyPoints) : name(name), loyaltyPoints(loyaltyPoints), totalAmountSpent(0.0), coffeeItem("Unkown", "Unknown", 0.0) {}
-
-    string getName() {
-        return name;
-    }
-
-    int getLoyaltyPoints() {
-        return loyaltyPoints;
-    }
-
-    double getTotalAmountSpent() {
-        return totalAmountSpent;
-    }
-
-    string getCoffeeItemName() {
-        return coffeeItem.getName();
-    }
-
-    friend void placeOrder(Customer& customer, CoffeeItem& coffee);
 };
 
 void placeOrder(Customer& customer, CoffeeItem& coffee) {
     customer.coffeeItem = coffee;
-    customer.totalAmountSpent += coffee.getPrice();
+    customer.addToTotalAmountSpent(coffee.getPrice());
 
+    int loyaltyPointsToAdd = 0;
     if (coffee.getSize() == "S") {
-        customer.loyaltyPoints += 1;
+        loyaltyPointsToAdd = 1;
     } else if (coffee.getSize() == "M") {
-        customer.loyaltyPoints += 2;
+        loyaltyPointsToAdd = 2;
     } else if (coffee.getSize() == "L") {
-        customer.loyaltyPoints +=3;
+        loyaltyPointsToAdd = 3;
     }
+    customer.addLoyaltyPoints(loyaltyPointsToAdd);
 }
 
-void LoyalityPointsChecker(Customer& customer, CoffeeItem& coffee) {
-    int loyalityPoints = customer.getLoyaltyPoints();
+void LoyaltyPointsChecker(Customer& customer, CoffeeItem& coffee) {
+    int loyaltyPoints = customer.getLoyaltyPoints();
     double price = coffee.getPrice();
 
-    if (loyalityPoints <= 5) {
-        price -= price * 2.5;
-    } else if (loyalityPoints <= 8) {
-        price -= price * 5;
-    } else if (loyalityPoints <= 10) {
-        price -= price * 7.5;
+    if (loyaltyPoints <= 5) {
+        price *= 0.975;
+    } else if (loyaltyPoints <= 8) {
+        price *= 0.95;
+    } else if (loyaltyPoints <= 10) {
+        price *= 0.925;
+    }
+    coffee.setPrice(price);
+}
+
+void printReceipt(const Customer& customer) {
+    static int receiptCount = 0;
+    receiptCount++;
+
+    const int width = 35;
+
+    string name = customer.getName();
+    if (!name.empty()) {
+        name[0] = toupper(name[0]);
     }
 
-    coffee.setPrice(price);
+    stringstream receiptNumberStream;
+    receiptNumberStream << " Chitanta numar : #" << setw(2) << setfill('0') << receiptCount;
+
+    cout << "|-----------------------------------|" << endl;
+    cout << "|" << setw(width) << left << receiptNumberStream.str() << "|" << endl;
+    cout << "|" << setw(width) << left << " Numele clientului: " + customer.getName() << "|" << endl;
+    cout << "|" << setw(width) << left << " Puncte de loialitate totale: " + to_string(customer.getLoyaltyPoints()) << "|" << endl;
+
+    stringstream stream;
+    stream << fixed << setprecision(2) << customer.getTotalAmountSpent() << " RON";
+    cout << "|" << setw(width) << left << " Suma totala cheltuita: " + stream.str() << "|" << endl;
+
+    cout << "|" << setw(width) << left << " Comanda de cafea: " + customer.getCoffeeItemName() << "|" << endl;
+    cout << "|-----------------------------------|" << endl;
 }
 
 int main() {
     string name;
-    
-    cout << "Introdu nume: ";
-    cin >> name;
-    cin.ignore();
+    bool validName = false;
+
+    while (!validName) {
+        cout << "Introdu nume: ";
+        cin >> name;
+
+        validName = true;
+        for (char c : name) {
+            if (!isalpha(c)) {
+                validName = false;
+                cout << "Numele introdus nu poate fi validat" << endl;
+                break;
+            }
+        }
+    }
+
+    if (!name.empty()) {
+        name[0] = toupper(name[0]);
+    }
 
     Customer customer(name, 0);
 
@@ -96,12 +123,12 @@ int main() {
 
     while (wantAnotherCoffee) {
         cout << "Selecteaza cafeaua:\n";
-        cout << "1. Cappuccino (S) - 4.99 RON\n";
+        cout << "1. Cappuccino (S) - 1.99 RON\n";
         cout << "2. Cappuccino (M) - 2.99 RON\n";
-        cout << "3. Cappuccino (L) - 2.99 RON\n";
-        cout << "4. Espresso (S) - 2.99 RON\n";
+        cout << "3. Cappuccino (L) - 3.99 RON\n";
+        cout << "4. Espresso (S) - 1.99 RON\n";
         cout << "5. Espresso (M) - 2.99 RON\n";
-        cout << "6. Espresso (L) - 2.99 RON\n";
+        cout << "6. Espresso (L) - 3.99 RON\n";
         cout << "0. Exit\n";
         cout << "Introdu optiune: ";
         cin >> coffeeChoice;
@@ -112,23 +139,21 @@ int main() {
 
         if (coffeeChoice >= 1 && coffeeChoice <= 6) {
             CoffeeItem coffeeOptions[] = {
-                CoffeeItem("Cappuccino (S)", "S", 4.99),
+                CoffeeItem("Cappuccino (S)", "S", 1.99),
                 CoffeeItem("Cappuccino (M)", "M", 2.99),
-                CoffeeItem("Cappuccino (L)", "L", 2.99),
-                CoffeeItem("Espresso (S)", "S", 2.99),
+                CoffeeItem("Cappuccino (L)", "L", 3.99),
+                CoffeeItem("Espresso (S)", "S", 1.99),
                 CoffeeItem("Espresso (M)", "M", 2.99),
-                CoffeeItem("Espresso (L)", "L", 2.99)
+                CoffeeItem("Espresso (L)", "L", 3.99)
             };
         placeOrder(customer, coffeeOptions[coffeeChoice - 1]);
 
-        cout << "Numele clientului: " << customer.getName() << endl;
-        cout << "Puncte de loialitate totale: " << customer.getLoyaltyPoints() << endl;
-        cout << "Suma totala cheltuita: " << customer.getTotalAmountSpent() << " RON" << endl;
-        cout << "Comanda de cafea: " << customer.getCoffeeItemName() << endl;
-
+        printReceipt(customer);
+        
         cout <<"Mai vrei o cafea? (1 - Da | 2 - Nu)";
         cin >> anotherOne;
         if (anotherOne == 2) {
+            cout << "Va multumim ca ati ales sa consumati o cafea de la noi! Va uram, o zi buna!";
             wantAnotherCoffee = false;
         }
     } else if (coffeeChoice != 0) {
---
+++ OOP/Lab4/readme.md
@@ -0,0 +1,32 @@
+# Coffee Shop Application
+
+## Overview
+This application simulates a basic coffee shop ordering system. It allows customers to place orders for different types of coffee, tracks the total amount spent, and calculates loyalty points based on the size of the coffee ordered. After each order, a receipt is printed, displaying the customer's name, the total amount spent, the loyalty points earned, and the details of the coffee order.
+
+## Features
+- Selection of coffee items with different sizes and prices.
+- Calculation of total amount spent on orders.
+- Loyalty points system based on the size of coffee ordered.
+- Input validation for customer names (accepts only alphabetic characters).
+- Printing of a formatted receipt after each order, including a unique receipt number.
+
+## Compilation and Execution
+This application is written in C++. To compile and run the program, you will need a C++ compiler like `g++`. 
+
+1. Save the code in a file named `coffee_shop.cpp`.
+2. Open a terminal or command prompt.
+3. Navigate to the directory containing `coffee_shop.cpp`.
+4. Compile the program: 'g++ CoffeeMachine.cpp -o coffee_shop'
+5. Run the executable: './coffee_shop'
+
+
+## Usage Example
+1. When prompted, enter a valid one-word, alphabetic name for the customer.
+2. Choose a coffee from the displayed menu by entering the corresponding number.
+3. After each order, a receipt is printed with the order details.
+4. You can order another coffee or exit the program.
+
+
+## Notes
+- The application does not persist data across runs.
+- Loyalty points and total amount spent are calculated for each session only.
