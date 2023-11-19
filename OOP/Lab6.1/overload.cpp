a27a14b0457c480676de8856b1b9d4fd932c72b4
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-19 15:40:47 +0200

    Update #0.1

--- OOP/Lab6.1/overload.cpp
+++ OOP/Lab6.1/overload.cpp
@@ -1,83 +1,47 @@
 #include <iostream>
-#include <cstring>
 
 using namespace std;
 
-class sir{
-    int lung;
-    char *psir;
+class Complex {
+    float Re, Im;
+public:
+    Complex(float Vr = 0, float Vi = 0) : Re(Vr), Im(Vi) {}
 
-    public:
-        sir(const char *s);
-        sir(int l=100);
-        sir(const sir&s);
-        void modifica();
-        void afisare();
-        sir& operator = (const sir& s);
-        sir operator + (const sir &s);
-        ~sir();
-};
+    Complex operator+(const Complex& z) const {
+        return Complex(Re + z.Re, Im + z.Im);
+    }
 
-sir::sir(const char *s) {
-    lung = strlen(s);
-    psir = new char[lung + 1];
-    strcpy(psir, s);
-};
+    Complex operator-(const Complex& z) const {
+        return Complex(Re - z.Re, Im - z.Im);
+    }
 
-sir::sir(int l) {
-    lung = l;
-    psir = new char[lung + 1];
-    *psir='\0';
-};
-
-sir::sir(const sir& s) {
-    lung = s.lung;
-    psir = new char[lung + 1];
-    strcpy(psir, s.psir);
-};
-
-void sir::modifica() {
-    strcpy(psir, "bau");
-};
-
-sir sir::operator+(const sir &s) {
-    sir aux;
-    aux.lung = lung + s.lung;
-    aux.psir = new char[aux.lung + 1];
-    strcpy(aux.psir, psir);
-    strcat(aux.psir, s.psir);
-    return aux;
-};
-
-sir::~sir() {
-    delete []psir;
-};
-
-sir& sir::operator = (const sir& s) {
-    if (this!=&s) {
-        delete this->psir;
-        this->lung=s.lung;
-        this->psir=new char[lung + 1];
-        strcpy(psir, s.psir);
+    Complex& operator=(const Complex& z) {
+        if (this == &z) {
+            return *this;
+        }
+        Re = z.Re;
+        Im = z.Im;
         return *this;
     }
-    return *this;
-};
 
-void sir::afisare() {
-    cout << psir;
+    void display() const {
+        cout << "(" << Re << " + " << Im << "i)" << endl;
+    }
 };
 
 int main() {
-    sir s1="cucu";
-    sir s2=s1;
-    s1.modifica();
-    sir s3="baba";
-    sir s4;
-    s4 = s3;
-    s3.modifica();
-    s4 = s1 + s2;
-    s4.afisare();
+    Complex c1(2, 3);
+    Complex c2(3, 4);
+    Complex c3;
+
+    c3 = c1 + c2;
+    c3.display();
+
+    c3 = c1 - c2;
+    c3.display();
+
+    Complex c4 = c3;
+    c4.display();
 
     return 0;
 }
