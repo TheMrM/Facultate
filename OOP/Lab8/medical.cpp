483f5e96e1d224585736b69888a2aa728ca46213
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-26 10:45:35 +0200

    Update medical
    It includes new functions for pacient age to set them in two categories. If patient is under 18 he is "Elev", over 54 "Pensionar".

--- OOP/Lab8/medical.cpp
+++ OOP/Lab8/medical.cpp
@@ -15,40 +15,103 @@ string getRandomElement(const vector<string>& vec) {
     return vec[rand() % vec.size()];
 }
 
-class Medic {
-    string nume;
-    string prenume;
-    string medicType;
-
-public:
-    Medic() : nume(getRandomElement(surnames)), prenume(getRandomElement(names)), medicType(getRandomElement(medicTypes)) {}
-
-    string getMedicInfo() const {
-        return "Dr. " + nume + " " + prenume + ", Specializare: " + medicType;
-    }
-};
-
 class Pacient {
     string nume;
     string prenume;
     string ocupatie;
+    int varsta;
 
 public:
-    Pacient() : nume(getRandomElement(surnames)), prenume(getRandomElement(names)), ocupatie(getRandomElement(occupations)) {}
+    Pacient() : nume(getRandomElement(surnames)), prenume(getRandomElement(names)), ocupatie(getRandomElement(occupations)), varsta(rand() % 67 + 4) {
+        if (varsta < 18) {
+            ocupatie = "Elev";
+        } else if (varsta < 55){
+            ocupatie = getRandomElement(occupations);
+        } else if (varsta >= 55){
+            ocupatie = "Pensionar";
+        }
+    }
 
     string getPacientInfo() const {
-        return "Pacient " + nume + " " + prenume + ", Ocupatia: " + ocupatie;
+        return "Pacient " + nume + " " + prenume + " in varsta de " + to_string(varsta) + " ani, Ocupatia: " + ocupatie;
+    }
+
+    int getVarsta() const {
+        return varsta;
+    }
+};
+class Medic {
+    string nume;
+    string prenume;
+    string medicType;
+    vector<Pacient> pacienti;
+
+public:
+    Medic() : nume(getRandomElement(surnames)), prenume(getRandomElement(names)), medicType(getRandomElement(medicTypes)) {
+        int numarPacienti = rand() % 10 + 1;
+        for (int i = 0; i < numarPacienti; ++i) {
+            pacienti.push_back(Pacient());
+        }
+    }
+
+    string getMedicInfo() const {
+        string info = "Dr. " + nume + " " + prenume + "\nSpecializare: " + medicType;
+        info += "\nNumar Pacienti: " + to_string(pacienti.size());
+        return info;
+    }
+
+    void afiseazaInfoPacienti() const {
+        for (const auto& pacient : pacienti) {
+            cout << pacient.getPacientInfo() << endl;
+        }
+    }
+
+    int numarPacientiSub18() const {
+        int count = 0;
+        for (const auto& pacient : pacienti) {
+            if (pacient.getVarsta() < 18) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    int numarPacientiPensionari() const {
+        int count = 0;
+        for (const auto& pacient : pacienti) {
+            if (pacient.getVarsta() >= 55) {
+                count++;
+            }
+        }
+        return count;
     }
 };
 
 int main() {
     srand(time(NULL));
 
-    Medic doctor1;
-    cout << doctor1.getMedicInfo() << endl;
+    int numarDoctori = rand() % 5 + 1;
+    vector<Medic> doctori;
 
-    Pacient pacient1;
-    cout << pacient1.getPacientInfo() << endl;
+    for (int i = 0; i < numarDoctori; i++) {
+        doctori.push_back(Medic());
+    }
+
+    for (int i = 0; i < numarDoctori; ++i) {
+        cout << "Doctor " << (i + 1) << " : " << endl;
+        cout << doctori[i].getMedicInfo() << endl;
+
+        int pacientiMinori = doctori[i].numarPacientiSub18();
+        cout << "Numarul de pacienti sub 18 ani: " << pacientiMinori << endl;
+
+        int pacientiPensionari = doctori[i].numarPacientiPensionari();
+        cout << "Numarul de pacienti pensionari: " << pacientiPensionari << endl;
+
+        cout << "Lista cu pacienti: " << endl;
+        doctori[i].afiseazaInfoPacienti();
+        cout << endl;
+    }
 
     return 0;
 }
