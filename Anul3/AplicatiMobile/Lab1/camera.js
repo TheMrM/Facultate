9e02b9f7419cbd5543117d33cad666c22b9e1e38
grasum <grasu_xxl_doja@yahoo.com>
2024-10-03 09:33:18 +0300

    Enhance camera functionality and fix canvas sizing
    Removed inline onclick handler in favor of event listener. Improved canvas size to match video dimensions and added functionality to download captured images with a unique filename.

--- Anul3/AplicatiMobile/Lab1/camera.js
+++ Anul3/AplicatiMobile/Lab1/camera.js
@@ -1,7 +1,10 @@
-var video = document.getElementById("video");
+const video = document.getElementById("video");
 
-video.addEventListener("touchstart", capteaza);
-video.addEventListener("mousedown", capteaza);
+let imageCounter = 1;
+video.addEventListener('playing', function () {
+    video.addEventListener("touchstart", capteaza);
+    video.addEventListener("mousedown", capteaza);
+});
 
 function on_cam_success(stream) {
     video.srcObject = stream;
@@ -11,14 +14,33 @@ function on_cam_error(err) {
     alert("error occurred"+err.message);
 }
 
-var constraints = { audio: false, video: true };
+const constraints = {audio: false, video: true};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam_success).catch(on_cam_error);
 
 function capteaza() {
-    var c = document.getElementById("canvas");
-    c.width = window.width;
-    c.height = window.height;
+    const c = document.getElementById("canvas");
+    c.width = video.videoWidth;
+    c.height = video.videoHeight;
 
-    var ctx = c.getContext("2d");
-    ctx.clearRect(0, 0, 640, 480);
+    const ctx = c.getContext("2d");
+    ctx.clearRect(0, 0, c.width, c.height);
+
+    ctx.drawImage(video, 0, 0, c.width, c.height);
 }
+
+function downloadImage() {
+    const c = document.getElementById("canvas");
+    const dataUrl = c.toDataURL("image/png");
+
+    const link = document.createElement("a");
+    link.download = 'myImage' + imageCounter + '.png';
+    link.href = dataUrl;
+    link.click();
+
+    imageCounter++;
+}
+
+document.querySelector('button').addEventListener('click', function () {
+    capteaza();
+    setTimeout(downloadImage, 100);
+});
