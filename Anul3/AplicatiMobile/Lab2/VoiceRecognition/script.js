83e5c8f2dd79620d537650453a3f8961ae2ad1e7
grasum <grasu_xxl_doja@yahoo.com>
2024-10-10 11:03:02 +0300

    Enhance voice recognition with UI and script functionality
    Added viewport meta tag, updated title, and included instructional text in the HTML. Introduced a new JavaScript script to handle touch events and speech recognition, displaying recognized words and their accuracy on the page.

--- Anul3/AplicatiMobile/Lab2/VoiceRecognition/script.js
+++ Anul3/AplicatiMobile/Lab2/VoiceRecognition/script.js
@@ -1,1 +1,37 @@
-some text
+document.addEventListener("touchstart", on_touch);
+document.addEventListener("mousedown", on_touch);
+
+let recognition;
+if ('webkitSpeechRecognition' in window) {
+    recognition = new webkitSpeechRecognition();
+}
+
+let recognition_start = false;
+recognition.lang = "ro";
+function on_touch() {
+    if(recognition_start) {
+        recognition.stop();
+        recognition_start = false;
+    } else {
+        recognition.start();
+        recognition_start = true;
+    }
+}
+function onend() {
+    recognition.stop();
+    recognition_start = false;
+}
+recognition.onend = onend;
+recognition.onsoundend = onend;
+recognition.onspeechend = onend;
+
+recognition.onresult = function (e) {
+    let textElement = document.getElementById("text");
+    if (textElement !== null) {
+        let resultIndex = e.resultIndex;
+        let transcript = e.results[resultIndex][0].transcript;
+        let confidence = e.results[resultIndex][0].confidence;
+
+        textElement.innerHTML += `Ati rostit cuvantul: <strong>${transcript}</strong>, acuratete: ${confidence.toFixed(2)}<br>`;
+    }
+}
